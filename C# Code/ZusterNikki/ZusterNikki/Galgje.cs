06bd894ba557cc627d4700f414d3199dda007a3b
Kevin Wieling <EGKangaroo@yahoo.com>
2018-05-07 11:47:18 +0200

    Added classes back in

---
+++ C# Code/ZusterNikki/ZusterNikki/Galgje.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeGalgje
+{
+    class Galgje
+    {
+        //fields
+        private Woord word;
+        private string revealedWord;
+        private int guessesLeft;
+        private List<string> wrongGuesses;
+        private List<string> allGuesses;
+        private bool win;
+
+        //properties
+        public Woord Word
+        {
+            get { return this.word; }
+        }
+
+        public string RevealedWord
+        {
+            get { return this.revealedWord; }
+        }
+
+        public int GuessesLeft
+        {
+            get { return this.guessesLeft; }
+        }
+
+        public List<string> WrongGuesses
+        {
+            get { return this.wrongGuesses; }
+        }
+
+        public List<string> AllGuesses
+        {
+            get { return this.allGuesses; }
+        }
+
+        public bool Win
+        {
+            get { return this.win; }
+        }
+
+        //constructors
+        public Galgje()
+        {
+            this.word = new Woord();
+            string revealedWord = "";
+
+            foreach (var character in Word.RevealedLetters)
+            {
+                revealedWord = revealedWord + character.ToString() + " ";
+            }
+
+            this.revealedWord = revealedWord;
+            this.guessesLeft = 10;
+            this.wrongGuesses = new List<string>();
+            this.allGuesses = new List<string>();
+            this.win = false;
+        }
+
+        //methods
+
+        //guess function
+        public void Guess(string letter)
+        {
+            bool alreadyGuessed = CheckAlreadyGuessed(letter);
+            string newRevealedWord = "";
+            if (alreadyGuessed == false)
+            {
+                allGuesses.Add(letter);
+                bool correctGuess = Word.CheckGuess(letter);
+                if(correctGuess == false)
+                {
+                    wrongGuesses.Add(letter);
+                    guessesLeft--;
+                }
+            }
+            foreach(var character in Word.RevealedLetters)
+            {
+                newRevealedWord = newRevealedWord + character.ToString() + " ";
+            }
+            revealedWord = newRevealedWord;
+            checkWin();
+        }
+
+        //checks whether a letter has already been guessed
+        public bool CheckAlreadyGuessed(string letter)
+        {
+            bool alreadyGuessed = false;
+
+            foreach (var guess in allGuesses)
+            {
+                if (guess == letter)
+                {
+                    alreadyGuessed = true;
+                }
+            }
+            return alreadyGuessed;
+        }
+
+        //checks whether the word has been fully guessed
+        private void checkWin()
+        {
+            if(Word.Letters.SequenceEqual(Word.RevealedLetters))
+            {
+                win = true;
+            }
+        }
+    }
+}
---
+++ C# Code/ZusterNikki/ZusterNikki/Woord.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeGalgje
+{
+    class Woord
+    {
+        //fields
+        private Char[] letters;
+        private Char[] revealedLetters;
+
+        //properties
+        public Char[] Letters
+        {
+            get { return this.letters; }
+        }
+
+        public Char[] RevealedLetters
+        {
+            get { return this.revealedLetters; }
+        }
+
+        //constructors
+        public Woord()
+        {
+            this.letters = ChooseRandomWord();
+            string revealedWord = "";
+
+            for(int i = 0; i < letters.Length ; i++)
+            {
+                revealedWord = revealedWord + "_";
+            }
+            this.revealedLetters = revealedWord.ToCharArray();
+        }
+
+        //methods
+
+        //picks a random word from the text file to be used when constructing a new instance of word
+        private Char[] ChooseRandomWord()
+        {
+            Random chooseWord = new Random();
+            FileStream filestream = new FileStream("WordFile.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(filestream);
+            List<string> allWords = new List<string>();
+            string line = reader.ReadLine();
+
+            while(line != null)
+            {
+                allWords.Add(line);
+                line = reader.ReadLine();
+            }
+
+            string chosenWord = allWords[chooseWord.Next(allWords.Count)];
+            Char[] charArray = chosenWord.ToCharArray();
+            return charArray;
+        }
+
+        //
+        public bool CheckGuess(string guess)
+        {
+            bool correctGuess = false;
+            int i = 0;
+            foreach(var letter in letters)
+            {
+                if(letter.ToString() == guess)
+                {
+                    correctGuess = true;
+                    revealedLetters[i] = Convert.ToChar(guess);
+                }
+                i++;
+            }
+            return correctGuess;
+        }
+    }
+}
