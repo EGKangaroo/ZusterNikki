8bb63d2ba4cf99a6bc44df98736d42131a0d08d6
Kevin Wieling <EGKangaroo@yahoo.com>
2018-05-07 11:42:49 +0200

    test again

--- C# Code/ZusterNikki/ZusterNikki/Inlog.cs
+++ C# Code/ZusterNikki/ZusterNikki/Inlog.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ZusterNikki
 {
-    public partial class Form1 : Form
+    public partial class Inlog : Form
     {
-        public Form1()
+        public Inlog()
         {
             InitializeComponent();
         }
